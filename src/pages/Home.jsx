8207c0ab1cf0b964d9436517e8bc84a14139714f
Imseongeun <sungyun0719@gmail.com>
2025-08-25 18:53:25 +0900

    feat/8 : 제발요

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import '../styles/shared.css'
 import '../styles/Home.css'
-import { useState } from 'react'
 import { useUserHealthInfo } from '../hooks/useUserHealthInfo'
 import logo from '/images/logo/logo.svg'
 import symbol from '/images/logo/logo-symbol.svg'
@@ -10,28 +9,7 @@ import FoodSuggest from '../components/home/FoodSuggest'
 import MenuGrid from '../components/home/MenuGrid'
 import Navbar from '../components/Navbar'
 
-// import { getOrCreateDeviceId } from '../utils/device';
-
-const DEVICE_KEY = 'mefo_device_id'
-
 function Home() {
-    // const [deviceId] = useState(() => getOrCreateDeviceId());
-    // console.log('deviceId:', deviceId);
-
-    // 마운트 시 1번만 실행: 하드코딩 값 쓰기 → 즉시 읽어서 로그
-    const [deviceId] = useState(() => {
-        const id = 'dev_TEST_FIXED_1234'
-        try {
-            localStorage.setItem(DEVICE_KEY, id)
-            const back = localStorage.getItem(DEVICE_KEY)
-            console.log('[DEV] wrote:', id, ' readBack:', back)
-        } catch (e) {
-            console.log('[DEV] localStorage failed:', e)
-        }
-        return id
-    })
-    console.log('[DEV] deviceId state:', deviceId)
-
     const { data: userInfo, isLoading, isError } = useUserHealthInfo()
 
     if (isLoading) return
