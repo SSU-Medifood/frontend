a0ab564377990519f80bb6131f84df548c6d4269
Imseongeun <sungyun0719@gmail.com>
2025-08-25 22:23:54 +0900

    feat/8 : 최종

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query" // Reac
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools" // DevTools
 import './index.css'
 import App from './App.jsx'
-import { nanoid } from 'nanoid'
-
-const DEVICE_KEY = 'mefo_device_id'
-try {
-  const newId = nanoid(24)
-  localStorage.setItem(DEVICE_KEY, newId)
-  console.log('[Device] NEW nanoid stored:', newId)
-} catch (e) {
-  console.error('[Device] storage error:', e)
-}
 
 // React Query Client 생성
 const queryClient = new QueryClient()
